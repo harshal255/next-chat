368b362c5105861abc57707641d213ec3bdc0f8b
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2025-04-22 22:19:33 +0530

    theme modification using next-theme

--- src/contexts/Providers.tsx
+++ src/contexts/Providers.tsx
@@ -3,20 +3,27 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 import { ChatProvider } from "./ChatProvider";
-
+import { ThemeProvider } from 'next-themes'
 
 
 export default function Providers({ children }: { children: ReactNode }) {
 
 
     return (
-        <ChakraProvider>
-            {/* user must authenticated */}
-            <SessionProvider>
-                <ChatProvider>
-                    {children}
-                </ChatProvider>
-            </SessionProvider>
-        </ChakraProvider>
+        <ThemeProvider
+            attribute="class" // Apply theme as a class on the HTML element
+            storageKey="chakra-ui-color-mode" // Custom key for localStorage
+            defaultTheme="system" // Default theme (optional)
+            enableSystem // Enable system theme detection
+        >
+            <ChakraProvider>
+                {/* user must authenticated */}
+                <SessionProvider>
+                    <ChatProvider>
+                        {children}
+                    </ChatProvider>
+                </SessionProvider>
+            </ChakraProvider>
+        </ThemeProvider>
     );
 }
